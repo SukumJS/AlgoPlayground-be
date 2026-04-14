4090afa77627371771ac09a4192bb797064076c1
watcharin28 <phon567gjo@gmail.com>
2026-04-14 12:28:36 +0700

    edit open env onrender

--- config/firebase.go
+++ config/firebase.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"strings"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
@@ -15,18 +16,37 @@ var Firestore *firestore.Client
 var AuthClient *auth.Client
 
 func InitFirebase() {
-
 	ctx := context.Background()
 
 	cred := os.Getenv("FIREBASE_CREDENTIAL")
-
 	if cred == "" {
 		log.Fatal("FIREBASE_CREDENTIAL not set in env")
 	}
 
-	opt := option.WithCredentialsFile(cred)
+	var opt option.ClientOption
 
-	app, err := firebase.NewApp(ctx, nil, opt)
+	if strings.HasPrefix(strings.TrimSpace(cred), "{") {
+		opt = option.WithCredentialsJSON([]byte(cred))
+		log.Println("Using credentials from JSON")
+	} else {
+		opt = option.WithCredentialsFile(cred)
+		log.Println("Using credentials from File")
+	}
+
+	// 1. ดึง Project ID มาจาก Environment 
+	projectID := os.Getenv("FIREBASE_PROJECT_ID")
+	if projectID == "" {
+		// ถ้าลืมใส่ใน .env หรือลืมใส่ใน Render ให้มันแจ้งเตือน
+		log.Println("WARNING: FIREBASE_PROJECT_ID is empty!")
+	}
+
+	// 2. ยัด Project ID ใส่ Config 
+	config := &firebase.Config{
+		ProjectID: projectID,
+	}
+
+	// 3. เปลี่ยนตรงนี้จาก nil ให้เป็น config แทน 
+	app, err := firebase.NewApp(ctx, config, opt)
 	if err != nil {
 		log.Fatalf("firebase init error: %v", err)
 	}
